d8d7359b6a27c91fb5980db8dd279c42965d3d30
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-10 21:55:54 +0800

    Add flameshot script for custom keyboard shortcut
    I can't believe I ignored this tool in the past
    This is very life changing

---
+++ flameshot/flameshot-full.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Automatically screenshot the whole screen and saved it in my clipboard
+# .local/bin/flameshot-full.sh
+
+flameshot full --clipboard
---
+++ flameshot/flameshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Trigger the flameshot screenshot with tools
+# .local/bin/flameshot.sh
+
+flameshot gui
