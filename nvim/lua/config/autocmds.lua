a1602bea3fa0b998270186bde9b4b96d6120c5f6
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-15 17:18:51 +0800

    Turn off all the italics in neovim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    for name, hl in pairs(vim.api.nvim_get_hl(0, {})) do
+      if hl.italic then
+        hl.italic = false
+        vim.api.nvim_set_hl(0, name, hl)
+      end
+    end
+  end,
+})
